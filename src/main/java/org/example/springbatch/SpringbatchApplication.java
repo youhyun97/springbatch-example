148755a2a186bedd1f5f68fc3925a90eba9c149b
youhyun97 <paris972006@gmail.com>
2021-07-13 19:55:13 +0900

    Add: simple batch

--- src/main/java/org/example/springbatch/SpringbatchApplication.java
+++ src/main/java/org/example/springbatch/SpringbatchApplication.java
@@ -3,6 +3,7 @@ package org.example.springbatch;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @EnableBatchProcessing // 배치 기능 활성화
 @SpringBootApplication
---
+++ src/main/java/org/example/springbatch/job/SampleJobConfiguration.java
@@ -0,0 +1,36 @@
+package org.example.springbatch.job;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j // log 사용을 위한 lombok 어노테이션
+@RequiredArgsConstructor // 생성자 DI를 위한 lombok 어노테이션
+@Configuration // Spring Batch의 모든 Job은 @Configuration 등록해서 사용.
+public class SampleJobConfiguration {
+    private final JobBuilderFactory jobBuilderFactory; // 생성자 DI 받음
+    private final StepBuilderFactory stepBuilderFactory; // 생성자 DI 받음
+
+    @Bean
+    public Job simpleJob() {
+        return jobBuilderFactory.get("simpleJob") // simpleJob이란 이름의 Batch Job 생성
+                .start(simpleStep1()) // simpleJob이 simpleStep1을 품고 있음.
+                .build();
+    }
+
+    @Bean
+    public Step simpleStep1() {
+        return stepBuilderFactory.get("simpleStep1") // simpleStep1 이름의 Batch Job 생성
+                .tasklet((contribution, chunkContext) -> { // step안에서 수행될 기능 명시
+                    log.info(">>>>> This is Step1");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+}
