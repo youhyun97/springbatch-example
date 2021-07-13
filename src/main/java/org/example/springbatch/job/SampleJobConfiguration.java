b1ba1e64452de45cc507d4abacb57e2fdf2a3168
youhyun97 <paris972006@gmail.com>
2021-07-13 20:10:57 +0900

    Add: jobParameters

--- src/main/java/org/example/springbatch/job/SampleJobConfiguration.java
+++ src/main/java/org/example/springbatch/job/SampleJobConfiguration.java
@@ -5,8 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -20,15 +22,17 @@ public class SampleJobConfiguration {
     @Bean
     public Job simpleJob() {
         return jobBuilderFactory.get("simpleJob") // simpleJob이란 이름의 Batch Job 생성
-                .start(simpleStep1()) // simpleJob이 simpleStep1을 품고 있음.
+                .start(simpleStep1(null)) // simpleJob이 simpleStep1을 품고 있음.
                 .build();
     }
 
     @Bean
-    public Step simpleStep1() {
+    @JobScope
+    public Step simpleStep1(@Value("#{jobParameters[requestDate]}") String requestDate) {
         return stepBuilderFactory.get("simpleStep1") // simpleStep1 이름의 Batch Job 생성
                 .tasklet((contribution, chunkContext) -> { // step안에서 수행될 기능 명시
                     log.info(">>>>> This is Step1");
+                    log.info(">>>>> requestDate = {}", requestDate);
                     return RepeatStatus.FINISHED;
                 })
                 .build();
